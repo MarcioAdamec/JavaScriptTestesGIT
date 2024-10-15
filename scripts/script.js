b5d7718acff48cce986bb597bb9dc17fb2761b0e
marcio.adamec <marcio.adamec@gmail.com>
2024-10-15 15:47:28 -0300

    voltando 0.0.1

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,49 @@
 
+let Tarefas = [];
 
 function salvarCompromisso(){
 
+    var CompromissInput = document.getElementById("idInput").value;
+    
+    Tarefas.push(CompromissInput);    
 
+    exibirTarefas();
+    
+}
+
+function exibirTarefas(){
+
+    let items = '';
+
+    Tarefas.forEach(element => {
+        
+         items += `${element} ` + "<p></p>"   ;
+
+    });
+
+    document.getElementById("pExibe").innerHTML = items;
+
+}
+
+function guardarItems(){
+
+        const arquivo = JSON.stringify(Tarefas);
+        localStorage.setItem("Repository", arquivo);
+        
+
+}
+
+function recuperarItems(){
+
+    let itemsRecuperados = localStorage.getItem("Repository");
+
+    console.log(itemsRecuperados);
+
+}
+
+function  apagarRepository(){
+
+    localStorage.clear();
 
 
 }
